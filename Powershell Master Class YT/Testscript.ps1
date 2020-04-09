a5f05772a1d67c4c1384896a8e247a35a531f969
codecracker17 <pa.keusch@hotmail.com>
2020-04-09 15:38:43 +0200

    some testing

--- Powershell Master Class YT/Testscript.ps1
+++ Powershell Master Class YT/Testscript.ps1
@@ -6,3 +6,12 @@ if (condition) {
 else {
     
 }
+
+git remote add origin https://github.com/codecracker17/MyPowershellExercisesDioCane.git
+git remote -v 
+git push -u origin master
+
+git remote add origin https://github.com/codecracker17/MyPowershellExercisesDioCane.git
+git push -u origin master "
+
+Write-Output "dio cane"
